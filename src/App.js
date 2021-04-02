7774e494461efb6fe39e0a17ad66b8e831ba1d1e
Ohidur Rahman <orahman@mettel.net>
2021-04-02 10:37:00 -0400

    Clean up junk

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>Knox</header>
+      <div id="d">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
