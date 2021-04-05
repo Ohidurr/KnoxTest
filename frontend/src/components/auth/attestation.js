79e427b42498eff089e1c9ca61f28c71fd7511f7
Ohidur Rahman <orahman@mettel.net>
2021-04-05 14:52:03 -0400

    add attestation to imei upload

--- frontend/src/components/auth/attestation.js
+++ frontend/src/components/auth/attestation.js
@@ -7,7 +7,7 @@ const attestation = (res) => {
     // identify the URL of your web server
     
     // through HTTP, send a nonce request to your web server
- res = HttpClient.getInstance().getNonce(URL_MDM_SERVER_MEASUREMENTS, ATTESTATION_KEY, REQUEST_ID);
+ res = res.getInstance().getNonce(URL_MDM_SERVER_MEASUREMENTS, ATTESTATION_KEY, REQUEST_ID);
     
 }
 
--- frontend/src/components/imeiUpload.js
+++ frontend/src/components/imeiUpload.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Axios from 'axios';
+import attestation from './auth/attestation'
 import '../css/imeiUpload.css'
 
 const { license_trial } = process.env;
@@ -10,7 +11,7 @@ const ImeiUpload = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        
+
         
     }
 
@@ -27,7 +28,7 @@ const ImeiUpload = () => {
                     <option value="Upload">Remove</option>
                 </select>
                 <input placeholder="IMEI" ></input>
-                <button class="submit" handleSubmit >Submit</button>
+                <button class="submit" handleSubmit={attestation}>Submit</button>
             </form>
         </div>
     )
