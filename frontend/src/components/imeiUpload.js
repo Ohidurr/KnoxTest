1717d92d449d730522f3d4d1d7de9f55f265fd68
Ohidur Rahman <orahman@mettel.net>
2021-04-07 11:50:17 -0400

    add knox library

--- frontend/src/components/imeiUpload.js
+++ frontend/src/components/imeiUpload.js
@@ -3,18 +3,23 @@ import Axios from 'axios';
 import attestation from './auth/attestation'
 import '../css/imeiUpload.css'
 
-const { license_trial } = process.env;
+const { Knox_Suite_license_trial, x_knox_apitoken } = process.env;
 
 console.log("test");
 
 const ImeiUpload = (res) => {
     
-    const  handleSubmit = (e) => {
-        e.preventDefault()
-       let res = `https://us-kcs-api.samsungknox.com/kcs/v1/rp/devices/upload`
+    const upload = () => {
         
 
+        let res = "https://us-kcs-api.samsungknox.com/kcs/v1/rp/devices/upload"
+
+    }
+    const  handleSubmit = (e) => {
+        e.preventDefault()
         
+
+    
     }
 
     const handleSelect = () => {
