50ac87315ba1e7b95bcf2c98e06f5daff77f2c5e
Ohidur Rahman <orahman@mettel.net>
2021-04-07 15:02:08 -0400

    add axios call for redriect

--- frontend/src/components/Signin.js
+++ frontend/src/components/Signin.js
@@ -1,12 +1,23 @@
 import React from 'react'
+const axios = require("axios")
 
 
-const Signin = () => {
+const Signin = (location) => {
     //redirects user to sign into samsung knox
+
+    const getUser = () => {
+        try{
+            axios.get("?client_Id=$`Id`& response_type=code&redirect_uri=$`http://localhost:3000`")
+        }catch(error){
+            console.log("error");
+        }
+    }
     return(
+
         <div>
+            
     <a href="https://account.samsung.com/accounts/v1/dfltThirdParty/signInGate">
-        
+
    <button>Sign In</button>
 </a>
         </div>
