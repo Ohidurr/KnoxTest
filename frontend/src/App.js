3884fbfc3b13d99e89091175a0e31a62d08781e8
Ohidur Rahman <orahman@mettel.net>
2021-04-07 14:21:07 -0400

    add sign in button that redirects to knox login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,13 @@ import { Switch, useLocation, Route } from "react-router-dom"
 
 import './App.css';
 import ImeiUpload from './components/ImeiUpload';
+import Signin from './components/Signin'
 
 const  App = () => {
   return (
     <div className="App">
    <header style={{fontSize: 'xx-large'}}>Knox Test</header>
+   <Signin />
     <ImeiUpload />
     </div>
   );
---
+++ frontend/src/components/Signin.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const Signin = () => {
+    //redirects user to sign into samsung knox
+    return(
+        <div>
+    <a href="https://account.samsung.com/accounts/v1/dfltThirdParty/signInGate">
+        
+   <button>Sign In</button>
+</a>
+        </div>
+    )
+}
+
+
+export default Signin
