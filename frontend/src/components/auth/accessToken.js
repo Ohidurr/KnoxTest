bca0943bb4259c238f982aeb73aabddba8ee2085
Ohidur Rahman <orahman@mettel.net>
2021-04-07 13:01:37 -0400

    random tokens

--- frontend/src/components/auth/accessToken.js
+++ frontend/src/components/auth/accessToken.js
@@ -1,3 +1,4 @@
+import { url } from 'node:inspector';
 import React from 'react'
 const knoxTokenLibraryJs = require('knox-token-library-js');
 const { accessToken } = proccess.env
@@ -6,6 +7,7 @@ const clientId = toString(accessToken);
 const signedClientId = knoxTokenLibraryJs.generateSignedClientIdentifierJWT("keys.json", clientId );
 
 const fetchToken = () => {
-    url = "https://us-kcs-api.samsungknox.com/ams/v1/users/accesstoken"
+    Base_Url = "https://us-kcs-api.samsungknox.com/ams/v1/users/accesstoken"
+    "https://us-kcs-api.samsungknox.com/ams/v1/users/accesstoken/$``"
     
 }
