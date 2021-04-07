fe042b94bb9681a345fa2e15c25a13e249f80965
Ohidur Rahman <orahman@mettel.net>
2021-04-07 11:55:16 -0400

    add fetch token

--- frontend/src/components/auth/accessToken.js
+++ frontend/src/components/auth/accessToken.js
@@ -1,3 +1,11 @@
 import React from 'react'
-
+const knoxTokenLibraryJs = require('knox-token-library-js');
 const { accessToken } = proccess.env
+const clientId = toString(accessToken);
+
+const signedClientId = knoxTokenLibraryJs.generateSignedClientIdentifierJWT("keys.json", clientId );
+
+const fetchToken = () => {
+    url = "https://us-kcs-api.samsungknox.com/ams/v1/users/accesstoken"
+    
+}
